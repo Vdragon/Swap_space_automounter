f7fe2e918c988b550ebeed6870515bc43af8c3d8
Ｖ字龍(Vdragon) <pika1021@gmail.com>
2012-12-23 03:43:27 +0800

    Add readme and initial script version

---
+++ README.md
@@ -0,0 +1,11 @@
+# 虛擬記憶體磁碟分割區自動掛載程式  
+Virtual memory partition automounter #
+
+ * For portable Linux system users to automatically mount virtual memory(or swap) partition to prevent hangs caused by main memory deficiency.
+ * Original idea comes from [\[SOLVED\] Automatic swap detection and mount - FedoraForum.org](http://forums.fedoraforum.org/showthread.php?t=264801)
+ * Thanks to stevea@forums.fedoraforum.org (not an email address!) for the original idea based on fdisk, I re-implement this using blkid tool instead.
+
+## 授權條款宣告  
+   License Declaration ##
+  GNU Lesser General Public License version 3.0 or later
+  http://www.gnu.org/licenses/lgpl.html
---
+++ mountAllVirtualMemory.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+# 		宣告執行 script 程式用的殼程式(shell)
+################## Header block ##################
+# Script 程式名稱 | Script Name
+# 		template.sh
+# 著作權宣告 | Copyright Declaration
+# 		Ｖ字龍(Vdragon) <pika1021@gmail.com> (c) 2012
+# 授權條款宣告 | License Declaration
+# 		GNU Lesser General Public License version 3.0 or later
+# 			http://www.gnu.org/licenses/lgpl.html
+# 傳回值定義 | Return Value Definition
+# 		0
+# 			正常結束
+# 已知問題 | Known Issues
+# 		Known issues is now on GitHub
+# 	
+# 修訂紀錄 | Changelog
+# 		Changelog is now on GitHub
+# 	
+############## Header block ended ##############
+
+######## Included files ########
+
+######## Included files ended ########
+
+######## File scope variable definitions ########
+
+######## File scope variable definitions ended ########
+
+######## Program ########
+# list all virtual_memory_partition's device file and turn into bash list format 
+for virtual_memory_partition in $(sudo blkid -t 'TYPE=swap' -o device | tr '\n' ' ')
+do
+	sudo swapon --discard ${virtual_memory_partition}
+done
+## 正常結束 script 程式
+exit 0
+######## Program ended ########
